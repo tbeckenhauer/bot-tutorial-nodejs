486ad8f62bc351b0b120aefa861d2841e7ee5980
Thomas Beckenhauer <tom@beckenhauer.me>
2017-09-01 21:40:15 -0400

    quick and dirty feature

--- bot.js
+++ bot.js
@@ -1,15 +1,44 @@
 var HTTPS = require('https');
-var cool = require('cool-ascii-faces');
+//var cool = require('cool-ascii-faces');
+var members = {};
+var workouts = [
+	'3 x 21 min',
+	'2x6k (4 x 3k)',
+	'5 x 11min',
+	'Blasters Distance',
+	'4 x 16 min',
+	'5 x 2k',
+	'4 x 10 min/5m r',
+	'10k (4x2.5k)',
+	'2 x 30 min / 5min rest',
+	'Blasters Time',
+	'3 x 20min',
+	'10,9,8,7'
+];
 
-var botID = process.env.BOT_ID;
+var cool = function (name) {
+	var current;
+	if(members.name) {
+	} else {
+		members.name = workouts;
+	}
+		
+	current = (members.name).shift();
+	(members.name).push(current);
+	return current;
+};
+
+
+var botID = '6ce321f96e7292e64d409a5bc3';
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy$/;
+      botRegex = /give me a workout$/;
+	console.log(request);
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
-    postMessage();
+    postMessage(request.name);
     this.res.end();
   } else {
     console.log("don't care");
@@ -41,6 +70,7 @@ function postMessage() {
         //neat
       } else {
         console.log('rejecting bad status code ' + res.statusCode);
+				console.log(res);
       }
   });
 
